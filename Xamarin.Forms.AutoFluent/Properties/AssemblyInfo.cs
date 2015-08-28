ed0aa903ffd0ed7f05eff5a8a5bcc92f562e09d8
Armin Sander <armin@replicator.org>
2015-08-28 20:40:02 +0200

    add Xamarin.Forms.AutoFluent project (as long the Command Line tool is not finished)

---
+++ Xamarin.Forms.AutoFluent/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Xamarin.Forms.AutoFluent")]
+[assembly: AssemblyDescription("Automatically generated fluent properties, events, and void methods for Xamarin.Forms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xamarin.Forms.AutoFluent")]
+[assembly: AssemblyCopyright("Copyright © 2015 Armin Sander")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
